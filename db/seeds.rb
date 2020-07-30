69aa68abaa54885d3a127a386065765648313d12
Arcoos2602 <cordonnier.thomas2@gmail.com>
2020-07-30 18:26:59 +0200

    complete with a basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+City.destroy_all
+Dog.destroy_all
+Dogsitter.destroy_all
+Stroll.destroy_all
+
+races = ["Bouledogue Anglais", "Coker Anglais", "Bouledogue Francais", "Berger Allemeand", "Carlin", "Bouvier Bernois", "Husky",
+         "Chihuahua", "Rottweiler", "Pitbull"]
+require 'faker'
+
+15.times do
+	city = City.create(city_name: Faker::Address.city)
+	dog = Dog.create(name: Faker::Creature::Dog.name, race: races[rand(0..(races.length))], city: city)
+	dog_sit = Dogsitter.create(name: Faker::Name.name, city: city)
+	Stroll.create(dog: dog, dogsitter: dog_sit)
+end
